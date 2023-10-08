2095f507ed8feb4f51ef2a85add4b503ba4b9ff0
Coatlicence <nojdaknchick@gmail.com>
2023-10-08 13:33:43 +0300

    1st Realisation of Tree Selfrecursing variant

---
+++ 22VP1_laba1_TVP/Tree.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+class Tree
+{
+	struct TreeElement
+	{
+		string word;
+
+		string rules
+	};
+
+	TreeElement Element;
+
+	vector<Tree> NextElements;
+};
