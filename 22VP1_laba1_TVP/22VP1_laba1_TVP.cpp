f05c15d12fbdd9ab8204ce65bdc5a0a89da99364
Coatlicence <nojdaknchick@gmail.com>
2023-10-08 21:21:35 +0300

    tree and language

--- 22VP1_laba1_TVP/22VP1_laba1_TVP.cpp
+++ 22VP1_laba1_TVP/22VP1_laba1_TVP.cpp
@@ -3,8 +3,21 @@
 #include "Language.h"
 #include "Tree.h"
 
+using namespace std;
+
+struct AlgorithmResult
+{
+    bool result = 0;
+
+    string word;
+
+    string rules;
+};
+
 int main()
 {
+    setlocale(LC_ALL, "ru");
+
     Dictionary terminal;
     terminal.AddSymbol("c");
     terminal.AddSymbol("d");
@@ -17,6 +30,7 @@ int main()
     myLanguage.AddRule("I", "ccd", "1");
     myLanguage.AddRule("I", "ccId", "2");
 
+    /*
     Tree t;
 
     auto root = t.RootElement;
@@ -27,5 +41,28 @@ int main()
     auto next = root->NextElements[1];
 
     next.AddElement("ccccdd", "1");
+    */
+
+    ///////////////////////////////////
+
+    cout << "Работу выполнили Резяков, Васильев, Князев. Группа 22ВП1" << endl << endl;
+    
+    cout << "Введите слово: " << endl;
+
+    string word;
+
+    cin >> word;
+
+    auto result = FindInLanguage(word, myLanguage);
+
+    cout << result.rules << endl;
 }
 
+AlgorithmResult FindInLanguage(string word, Language lang)
+{
+    AlgorithmResult result;
+
+    ///
+
+    return result;
+}
